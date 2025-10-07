e5302338d51f66c17b1c733f818080ab3f5b2882
Sanika Tiwari <thesanikatiwari@gmail.com>
2025-10-07 11:28:31 +0800

    button thing and tsukasa

---
+++ script.js
@@ -0,0 +1,12 @@
+function darkmode(){
+    const body = document.body;
+    const button = document.getElementById("darkmodebutton");
+
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+    button.innerText = "Tsukasa mode";
+  } else {
+    button.innerText = "An mode";
+  }
+}
