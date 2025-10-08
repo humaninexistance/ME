9a7d169f8e537e0ec5f679760d3023711967e75b
Sanika Tiwari <thesanikatiwari@gmail.com>
2025-10-08 16:54:36 +0800

    idk gng

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function darkmode(){
     body.classList.toggle("dark");
 
     if (body.classList.contains("dark")) {
-    button.innerText = "Tsukasa mode";
+    button.innerText = "Tsukasa mode!";
   } else {
-    button.innerText = "An mode";
+    button.innerText = "An mode!";
   }
 }
