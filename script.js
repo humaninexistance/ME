d6923bd2ea9b83be042cb1297aad97ded65dafc3
Sanika Tiwari <thesanikatiwari@gmail.com>
2025-10-11 16:38:25 +0800

    edited the nums and changed route

--- script.js
+++ script.js
@@ -26,175 +26,175 @@ function changeImageHover1(){
   document.getElementById("star1").src="odo.png";
 }
 function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+  document.getElementById("star1").src="assets/icons+chibis/star.png";
 }
 //star 2
 function changeImageHover2(){
   document.getElementById("star2").src="odo.png";
 }
 function changeImageLeave2(){
-  document.getElementById("star2").src="star.png";
+  document.getElementById("star2").src="assets/icons+chibis/star.png";
 }
 //star 3
 function changeImageHover3(){
   document.getElementById("star3").src="odo.png";
 }
 function changeImageLeave3(){
-  document.getElementById("star3").src="star.png";
+  document.getElementById("star3").src="assets/icons+chibis/star.png";
 }
 //star 4
 function changeImageHover4(){
   document.getElementById("star4").src="odo.png";
 }
 function changeImageLeave4(){
-  document.getElementById("star4").src="star.png";
+  document.getElementById("star4").src="assets/icons+chibis/star.png";
 }
 //star 5
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover5(){
+  document.getElementById("star5").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave5(){
+  document.getElementById("star5").src="assets/icons+chibis/star.png";
 }
 //star6
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover6(){
+  document.getElementById("star6").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave6(){
+  document.getElementById("star6").src="assets/icons+chibis/star.png";
 }
 //star7
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover7(){
+  document.getElementById("star7").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave7(){
+  document.getElementById("star7").src="assets/icons+chibis/star.png";
 }
 //star 8
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover8(){
+  document.getElementById("star8").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave8(){
+  document.getElementById("star8").src="assets/icons+chibis/star.png";
 }
 //star9
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover9(){
+  document.getElementById("star9").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave9(){
+  document.getElementById("star9").src="assets/icons+chibis/star.png";
 }
 //star10
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover10(){
+  document.getElementById("star10").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave10(){
+  document.getElementById("star10").src="assets/icons+chibis/star.png";
 }
 //star11
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover11(){
+  document.getElementById("star11").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave11(){
+  document.getElementById("star11").src="assets/icons+chibis/star.png";
 }
 //star12
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover12(){
+  document.getElementById("star12").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave12(){
+  document.getElementById("star12").src="assets/icons+chibis/star.png";
 }
 //star13
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover13(){
+  document.getElementById("star13").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave13(){
+  document.getElementById("star13").src="assets/icons+chibis/star.png";
 }
 //star14
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover14(){
+  document.getElementById("star14").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave14(){
+  document.getElementById("star14").src="assets/icons+chibis/star.png";
 }
 //star15
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover15(){
+  document.getElementById("star15").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave15(){
+  document.getElementById("star15").src="assets/icons+chibis/star.png";
 }
 //star16
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover16(){
+  document.getElementById("star16").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave16(){
+  document.getElementById("star16").src="assets/icons+chibis/star.png";
 }
 //star17
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover17(){
+  document.getElementById("star17").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave17(){
+  document.getElementById("star17").src="assets/icons+chibis/star.png";
 }
 //star 18
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover18(){
+  document.getElementById("star18").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave18(){
+  document.getElementById("star18").src="assets/icons+chibis/star.png";
 }
 //star 19
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover19(){
+  document.getElementById("star19").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave19(){
+  document.getElementById("star19").src="assets/icons+chibis/star.png";
 }
 //star 20
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover20(){
+  document.getElementById("star20").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave20(){
+  document.getElementById("star20").src="assets/icons+chibis/star.png";
 }
 //star 21
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover21(){
+  document.getElementById("star21").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave21(){
+  document.getElementById("star21").src="assets/icons+chibis/star.png";
 }
 //star 22
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover22(){
+  document.getElementById("star22").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave22(){
+  document.getElementById("star22").src="assets/icons+chibis/star.png";
 }
 //star 23
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover23(){
+  document.getElementById("star23").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave23(){
+  document.getElementById("star23").src="assets/icons+chibis/star.png";
 }
 //star 24
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover24(){
+  document.getElementById("star24").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave24(){
+  document.getElementById("star24").src="assets/icons+chibis/star.png";
 }
 //star 25
-function changeImageHover1(){
-  document.getElementById("star1").src="odo.png";
+function changeImageHover25(){
+  document.getElementById("star25").src="odo.png";
 }
-function changeImageLeave1(){
-  document.getElementById("star1").src="star.png";
+function changeImageLeave25(){
+  document.getElementById("star25").src="assets/icons+chibis/star.png";
 }
 //IM FREEEE
 //FINALLY
