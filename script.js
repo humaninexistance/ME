452c38278a8b4030c4fda8cd82b1aafbec072259
Sanika Tiwari <thesanikatiwari@gmail.com>
2025-10-11 16:19:52 +0800

    the hover thing
    im like 90% sure i was supposed to use a for loop but idk how so uh

--- script.js
+++ script.js
@@ -21,9 +21,181 @@ rows.forEach(row => {
   row.style.setProperty("--row-gap", `${randomRowGap}px`);
 });
 
+// for star 1
 function changeImageHover1(){
   document.getElementById("star1").src="odo.png";
 }
 function changeImageLeave1(){
   document.getElementById("star1").src="star.png";
 }
+//star 2
+function changeImageHover2(){
+  document.getElementById("star2").src="odo.png";
+}
+function changeImageLeave2(){
+  document.getElementById("star2").src="star.png";
+}
+//star 3
+function changeImageHover3(){
+  document.getElementById("star3").src="odo.png";
+}
+function changeImageLeave3(){
+  document.getElementById("star3").src="star.png";
+}
+//star 4
+function changeImageHover4(){
+  document.getElementById("star4").src="odo.png";
+}
+function changeImageLeave4(){
+  document.getElementById("star4").src="star.png";
+}
+//star 5
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star6
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star7
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star 8
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star9
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star10
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star11
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star12
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star13
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star14
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star15
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star16
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star17
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star 18
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star 19
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star 20
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star 21
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star 22
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star 23
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star 24
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//star 25
+function changeImageHover1(){
+  document.getElementById("star1").src="odo.png";
+}
+function changeImageLeave1(){
+  document.getElementById("star1").src="star.png";
+}
+//IM FREEEE
+//FINALLY
+//my back hurts.
